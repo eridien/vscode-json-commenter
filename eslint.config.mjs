bc68e987b5a7dca3682644a3a63d745399dc3fcf
Mark Hahn <mark@hahnca.com>
2025-05-12 14:37:55 -0700

    bkup

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Define __dirname for ES module
+
+export default [{
+    files: ["**/*.ts"],
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module",
+        parserOptions: {
+            project: "./tsconfig.json",
+            tsconfigRootDir: __dirname, // Use the derived __dirname
+        },
+    },
+    rules: {
+        "require-await": "error",
+        "no-return-await": "error",
+        "@typescript-eslint/await-thenable": "error",
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "import",
+            format: ["camelCase", "PascalCase"],
+        }],
+        curly: "off",
+        eqeqeq: "off",
+        "no-throw-literal": "warn",
+        semi: "warn",
+        "no-unused-vars": "off",
+    },
+}];
