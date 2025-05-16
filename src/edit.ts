ab59af6e6385b8377c234ebf08db9d973691014c
Mark Hahn <mark@hahnca.com>
2025-05-16 16:05:41 -0700

    finished edit.ts to point of setting editingBlock

--- src/edit.ts
+++ src/edit.ts
@@ -6,50 +6,59 @@ const { log, start, end } = utils.getLog('edit');
 export const ID_WIDTH = 6;
 
 interface BlockLine {
-  startQuote: number;
-  type:       number;
-  padWidth:   number;
-  endQuote:   number;
+  lineLen:    number;
+  indentLen:  number;
+  id:         number;
+  border:     boolean;
+  lastLine:   boolean;
+  padLen:     number;
   text:       string;
+  hasComma:   boolean;
 }
 
 interface Block {
-  startLine: number;
-  endLine:   number;
-  startText: number;
-  endText:   number;
-  text:      string;
-  blocklines:  Array<BlockLine>;
+  startLine:  number;
+  endLine:    number;
+  startText:  number;
+  endText:    number;
+  text:       string;
+  blocklines: Array<BlockLine>;
 }
-
-//"    "​​​​​‍​‍":"  Click here and start typing.              ","
-// "[\u200B\u200C\u200D\u2060]{6}([\u200B\u200C\u200D\u2060])([\u200B\u200C\u200D\u2060])":(.*?)"\/g/
 
 let editingBlock: Block | null | undefined = null;
 
-const invChrRegEx = /[\u200B\u200C\u200D\u2060]/;
-const lineRegEx = new RegExp(`"[\\u200B\\u200C\\u200D\\u2060]{${ID_WIDTH}}` +
-                             `([\\u200B\\u200C\\u200D\\u2060])` +
-                             `([\\u200B\\u200C\\u200D\\u2060])":"(.*?)"/g`);
+const oneInvChar  = '[\u200B\u200C\u200D\u2060]';
+const invChrRegEx = new RegExp(oneInvChar);
+const lineRegEx   = new RegExp(
+  `^( *?)"(${oneInvChar}{${ID_WIDTH}})(${oneInvChar})` +
+        `(${oneInvChar})":"(.*?)"(\,?)$`);
+
+// "    \"​​​​​‍​‍\":\"  Click here and start typing.              \","
+// /^( *?)([​‌‍⁠]{6})([​‌‍⁠])([​‌‍⁠])":"(.*?)"(,?)$/
 
 function getBlockLine(document: vscode.TextDocument, lineNumber: number, 
                           line: vscode.TextLine | null) : BlockLine | null {
   if (!line) return null;
   const groups = lineRegEx.exec(line.text);
   if(!groups) return null;
+  const lineType = utils.inv2num(groups[3]);
   const blkLine = {
-    startQuote: groups.index as number,
-    type:       utils.inv2num(groups[1]),
-    padWidth:   utils.inv2num(groups[2]),
-    endQuote:   groups.index + groups[0].length,
-    text:       groups[3],
+    lineLen:    groups[0].length,
+    indentLen:  groups[1].length,
+    id:         utils.invBase4ToNumber(groups[2]),
+    border:     !!(lineType & 0x2),
+    lastLine:   !!(lineType & 0x1),
+    padLen:     utils.inv2num(groups[4]),
+    text:       groups[5],
+    hasComma:   groups[6] === ',',
   };
-  if(blkLine.startQuote != line.firstNonWhitespaceCharacterIndex) return null;
-  if(groups[0].length   != ID_WIDTH + blkLine.text.length + 6)    return null;
-  const padStr = ' '.repeat(blkLine.padWidth);
-  if(!blkLine.text.startsWith(padStr))                            return null;
-  if(!blkLine.text.endsWith(  padStr))                            return null;
-  if(blkLine.startQuote + groups[0].length != line.text.length)   return null;
+  if(!blkLine.border) {
+    const padStr = ' '.repeat(blkLine.padLen);
+    if(!blkLine.text.startsWith(padStr)) return null;
+    if(!blkLine.text.endsWith(  padStr)) return null;
+  }
+  blkLine.text = blkLine.text.slice(blkLine.padLen,
+                 blkLine.text.length - blkLine.padLen);
   return blkLine; 
 }
 
@@ -58,38 +67,37 @@ function getBlock(document: vscode.TextDocument,
   let lineNumber = clickPos.line;
   const line     = document.lineAt(lineNumber);
   if(!line || !invChrRegEx.test(line.text)) return undefined;
-  const blkLine = getBlockLine(document, lineNumber, line);
-  if(!blkLine) return null;
-  const blocklines = [blkLine];
+  const blockLine = getBlockLine(document, lineNumber, line);
+  if(!blockLine) return null;
+  const blocklines = [blockLine];
   let lineNum = lineNumber;
+  let blkLine: BlockLine | null | undefined;
   do{
-    const line    = document.lineAt(--lineNum);
-    const blkLine = getBlockLine(document, lineNum, line);
+    const line  = document.lineAt(--lineNum);
+    blkLine = getBlockLine(document, lineNum, line);
     if(blkLine) blocklines.unshift(blkLine);
   } while(blkLine);
-  const startLine = lineNum + 1;
+  const startLine = lineNum+1;
   lineNum = lineNumber;
   do{
-    const line    = document.lineAt(++lineNum);
-    const blkLine = getBlockLine(document, lineNum, line);
+    const line  = document.lineAt(++lineNum);
+    blkLine = getBlockLine(document, lineNum, line);
     if(blkLine) blocklines.push(blkLine);
   } while(blkLine);
-  const endLine = lineNum - 1;
+  const endLine = lineNum-1;
   let text = '';
   const firstLine = blocklines[0];
   const textLen   = firstLine.text.length;
   for(let i = 0; i < blocklines.length; i++) {
     const blkLine = blocklines[i];
     if(blkLine.text.length != textLen) return null;
-    if(i  < blocklines.length-1 && (blkLine.type % 1) != 0) return null;
-    if(i == blocklines.length-1 && (blkLine.type % 1) != 1) return null;
-    text += blkLine.text.slice(blkLine.padWidth, 
-                               blkLine.text.length - blkLine.padWidth) + ' ';
+    if(i  < blocklines.length-1 &&  blkLine.lastLine) return null;
+    if(i == blocklines.length-1 && !blkLine.lastLine) return null;
+    if(!blkLine.border) text += blkLine.text + ' ';
   }
   text = text.trim();
-  const startText = firstLine.startQuote + ID_WIDTH + 
-                    firstLine.padWidth + 6;
-  const endText   = startText + firstLine.text.length - firstLine.padWidth * 2;
+  const startText = firstLine.indentLen + 1 + ID_WIDTH + 5 + firstLine.padLen;
+  const endText   = startText + firstLine.text.length;
   return { startLine, endLine, startText, endText, text, blocklines };
 }
 
@@ -101,7 +109,7 @@ function startEditing() {
 
 export function stopEditing() {
   if (!editingBlock) return;
-  // clear decorations
+  // clear and dispose decorations
   editingBlock = null;
   log('info', 'Editing ended.');
 }
