1ce1ef061a0061944c389d413aca48086672504d
Mark Hahn <mark@hahnca.com>
2025-05-15 23:59:23 -0700

    bkup

--- src/edit.ts
+++ src/edit.ts
@@ -2,43 +2,34 @@ import vscode     from 'vscode';
 import * as utils from './utils';
 const { log, start, end } = utils.getLog('edit');
 
-interface BoxLocation {
+interface BlockLine {
+  startQuote: number;
+  startFull:  number;
+  padWidth:   number;
+  textWidth:  number;
+  endQuote:   number;
+  text:       string;
+  lineId:     string;
+  lineType:   string;
+}
+
+interface Block {
   startLine: number;
   endLine:   number;
   startChar: number;
   endChar:   number;
+  text:      string;
+  lines: Array<BlockLine>;
 }
 
-interface BoxLine {
-  indentWidth: number;
-  lineId: string;
-  startChar: number;
-  endChar: number;
-  padWidth: number;
-  text: string;
-  lineType: string;
-}
-
-let editing = false;
-
-function startEditing() {
-  if (editing) return;
-  editing = true;
-  log('info', 'Editing started.');
-}
-
-export function stopEditing() {
-  if (!editing) return;
-  editing = false;
-  log('info', 'Editing ended.');
-}
+let editingBlock: Block | null = null;
 
 const lineRegEx = new RegExp(`"([\\u200B\\u200C\\u200D\\u2060]+)` +
                               `([\\u200B\\u200C\\u200D\\u2060]{3})":(.*?)` +
                               `([\\u200B\\u200C\\u200D\\u2060]+)"/g`);
 
 function ChkLine(document: vscode.TextDocument, 
-                 lineNumber: number): BoxLine | null {
+                 lineNumber: number): BlockLine | null {
   const line   = document.lineAt(lineNumber);  if (!line)   return null;
   const groups = lineRegEx.exec(line.text);    if (!groups) return null;
   const [lineId, padWidthStr, fullText, lineTypeInv] = groups.slice(1);
@@ -51,21 +42,75 @@ function ChkLine(document: vscode.TextDocument,
   return { indentWidth, lineId, startChar, endChar, padWidth, text, lineType };
 }
 
-function getBlockClicked(document: vscode.TextDocument, 
-                         clickPos: vscode.Position): BoxLocation | null {
+function getRawBlock(document: vscode.TextDocument, 
+                         clickPos: vscode.Position): RawBlock | null {
   let lineNumber = clickPos.line;
   const lineData = ChkLine(document, lineNumber);
   if( !lineData) return null;
-  const { startChar, endChar, text, lineType } = lineData;
-  const textLines = [text];
-  let firstLineNumber = lineNumber;
-  let lastLineNumber  = lineNumber;
-
+  const { startChar, endChar } = lineData;
+  const lines = [lineData];
+  let lineNum = lineNumber;
   do{
-    const lineData = ChkLine(document, lineNumber);
-  } while(0);
-  
-  return null;
+    const lineData = ChkLine(document, --lineNum);
+    if(lineData) lines.unshift(lineData);
+  } while(lineData);
+  const startLine = lineNum + 1;
+  lineNum = lineNumber;
+  do{
+    const lineData = ChkLine(document, ++lineNum);
+    if(lineData) lines.push(lineData);
+  } while(lineData);
+  const endLine   = lineNum - 1;
+  return { startLine, endLine, startChar, endChar, lines };
+}
+
+/*
+interface RawBlock {
+  startLine: number;
+  endLine:   number;
+  startChar: number;
+  endChar:   number;
+  lines: Array<BlockLine>;
+}
+
+interface Block {
+  startLine: number;
+  endLine:   number;
+  startChar: number;
+  endChar:   number;
+  text:      string;
+  lines: Array<BlockLine>;
+}
+*/
+
+function fixBlock(rawBlock: RawBlock): Block | null {
+  if (!rawBlock) return null;
+  const text = rawBlock.lines.map(line => line.text.trim()).join(' ');
+  for(let lineNum = rawBlock.startLine; lineNum <= rawBlock.endLine; lineNum++) {
+    const line = rawBlock.lines[lineNum - rawBlock.startLine];
+    const { startChar, text } = line;
+    if(startChar !== rawBlock.startChar ||
+         endChar !== rawBlock.endChar) {
+      const lineData = rawBlock.lines[lineNum - rawBlock.startLine];
+      const { startChar, endChar } = lineData;
+      const line = document.lineAt(lineNum);
+      const range = new vscode.Range(lineNum, startChar, lineNum, endChar);
+    }
+  }
+  return { ...rawBlock, text };
+}
+
+function startEditing() {
+  if (!editingBlock) return;
+  // editor.setDecorations(decorationType, [range]);
+  log('info', 'Editing started.');
+}
+
+export function stopEditing() {
+  if (!editingBlock) return;
+  // clear decorations
+  editingBlock = null;
+  log('info', 'Editing ended.');
 }
 
 export function selectionChanged(event:vscode.TextEditorSelectionChangeEvent) {
@@ -74,8 +119,9 @@ export function selectionChanged(event:vscode.TextEditorSelectionChangeEvent) {
         kind === vscode.TextEditorSelectionChangeKind.Mouse) {
     const clickPos = selections[0].active;
     if (!clickPos) return;
-    if(getBlockClicked(editor.document, clickPos)) {
-      startEditing();
+    const rawBlock = getRawBlock(editor.document, clickPos);
+    if(rawBlock) {
+      startEditing(rawBlock);
       log('info', `Selection changed at ${clickPos.line}:${clickPos.character}`);
     }
   }
