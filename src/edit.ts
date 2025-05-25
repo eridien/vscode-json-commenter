78d5f13b142f7cadc57922dadffa0c10d88fb99f
Mark Hahn <mark@hahnca.com>
2025-05-24 18:05:05 -0700

    clnup

--- src/edit.ts
+++ src/edit.ts
@@ -375,9 +375,6 @@ export async function selectionChanged(
                           event:vscode.TextEditorSelectionChangeEvent) {
   const {textEditor:editor, selections, kind} = event;
   const document = editor.document;
-
-  log(utils.parseJsonWithComments(document.getText()));
-
   if(selections.length == 1 && selections[0].isEmpty &&
         kind === vscode.TextEditorSelectionChangeKind.Mouse) {
     if(editArea) clrDecoration();
